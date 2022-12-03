99013eabbb9bff23ba550119b1737d0f590d143e
Mahad Saleem <ms.asd871@gmail.com>
2022-12-03 17:29:52 +0500

    initial Node Push <3

---
+++ BTree.h
@@ -0,0 +1,58 @@
+#pragma once
+
+
+template <class T>
+
+class Node
+{
+public:
+
+	T data;
+	Node* NextNode;
+
+	Node()
+	{
+		NextNode = NULL;
+	}
+
+	Node(T d, Node* n)
+	{
+		setval(d);
+		setNext(n);
+	}
+
+	Node(T d)
+	{
+		setval(d);
+		NextNode = NULL;
+
+	}
+
+	void setval(T d)
+	{
+		val = d;
+	}
+
+	void setNext(Node* n)
+	{
+		NextNode = n->NextNode;
+
+	}
+
+	T getval()
+	{
+		return val;
+	}
+
+	Node* getNext()
+	{
+		return this->NextNode;
+	}
+
+
+};
+
+template <class T>
+
+
+
